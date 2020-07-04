42667c8a71eed9a25289baa97cd56fdeb82cf62d
test <smeaton@sfu.ca>
2020-07-04 16:30:53 -0700

    Branch 5: adds background of each inspection based off hazard level. Has an on click listener for each list item.
    Needs to integrate model package.

--- app/src/main/java/ca/sfu/cmpt_276_project/SingleRestaurantActivity.java
+++ app/src/main/java/ca/sfu/cmpt_276_project/SingleRestaurantActivity.java
@@ -3,6 +3,7 @@ package ca.sfu.cmpt_276_project;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
@@ -11,6 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -25,22 +27,26 @@ import java.util.List;
 Shows the details of a single restaurant.Shows restaurant name, address, and coordinates.
 Has a scroll-able list that show all it's inspections by recency.
 Each inspection on the list should show: #critical issues, #non-critical issues, how long
-since inspection, and an icon with hazard level and colour.
+since inspection, and an icon with hazard level and colour. When clicked, an inspection will take
+the user to IndividualInspectionActivity.
  */
 class Inspection{
     private int numCritIssues;
     private int numNonCritIssues;
     private String timeSinceInspection;
     private int icon;
+    private String hazardLevel;
 
     public Inspection(int numCritIssues,
                       int numNonCritIssues,
                       String timeSinceInspection,
-                      int icon){
+                      int icon,
+                      String hazardLevel){
         this.numCritIssues = numCritIssues;
         this.numNonCritIssues = numNonCritIssues;
         this.timeSinceInspection = timeSinceInspection;
         this.icon = icon;
+        this.hazardLevel = hazardLevel;
     }
 
     public int getNumCritIssues() {
@@ -73,6 +79,10 @@ class Inspection{
 
     public void setIcon(int icon) {
         this.icon = icon;
+    }
+
+    public String getHazardLevel() {
+        return hazardLevel;
     }
 }
 class Restaurant{
@@ -158,14 +168,38 @@ public class SingleRestaurantActivity extends AppCompatActivity {
         inspections.add(new Inspection(2,
                 1,
                 "20 days",
-                R.drawable.high_hazard));
+                R.drawable.high_hazard,
+                "high"));
         inspections.add(new Inspection(0,
                 0,
                 "1 year",
-                R.drawable.low_hazard));
+                R.drawable.low_hazard,
+                "low"));
         populateInspectionsList();
+        registerOnClick();
 
 
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        populateInspectionsList();
+        registerOnClick();
+    }
+
+
+    private void registerOnClick() {
+        ListView list = findViewById(R.id.inspectionsListView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = SingleInspectionActivity.makeIntent(
+                        SingleRestaurantActivity.this);
+                startActivity(intent);;
+
+            }
+        });
     }
 
     private void populateInspectionsList() {
@@ -193,17 +227,31 @@ public class SingleRestaurantActivity extends AppCompatActivity {
                         parent,
                         false);
             }
-            Inspection currentInspectiion = inspections.get(position);
+            Inspection currentInspection = inspections.get(position);
 
             ImageView imageView = itemView.findViewById(R.id.hazardicon);
-            imageView.setImageResource(currentInspectiion.getIcon());
+            imageView.setImageResource(currentInspection.getIcon());
 
             TextView numCritIssueText = itemView.findViewById(R.id.numCritIssuesValue);
-            numCritIssueText.setText("" + currentInspectiion.getNumCritIssues());
+            numCritIssueText.setText("" + currentInspection.getNumCritIssues());
 
             TextView numNonCritIssueText = itemView.findViewById(R.id.numNonCritVal);
-            numNonCritIssueText.setText("" + currentInspectiion.getNumNonCritIssues());
-            //TODO set background based on hazard level
+            numNonCritIssueText.setText("" + currentInspection.getNumNonCritIssues());
+
+            TextView inspectionDateText = itemView.findViewById(R.id.inspectionDateValue);
+            inspectionDateText.setText(currentInspection.getTimeSinceInspection());
+
+            if(currentInspection.getHazardLevel() == "low"){
+                itemView.setBackgroundColor(Color.GREEN);
+            }
+            else if(currentInspection.getHazardLevel() == "medium"){
+                itemView.setBackgroundColor(Color.YELLOW);
+            }
+            else{
+                itemView.setBackgroundColor(Color.RED);
+            }
+
+
             //TODO set onclick
             return itemView;
         }
