55e01b3ce433afa571c4bb6a4c9bd0f62c33b0db
test <smeaton@sfu.ca>
2020-07-03 17:29:26 -0700

    Branch 5: adds temporary Inspection class
    Needs a ListView to show the restaurant inspections. Will display #critical issues, #non-critical issues as ints. Needs to display time since last inspection as a string. Needs to change colour and display an icon based off of inspection results.

--- app/src/main/java/ca/sfu/cmpt_276_project/SingleRestaurantActivity.java
+++ app/src/main/java/ca/sfu/cmpt_276_project/SingleRestaurantActivity.java
@@ -13,12 +13,63 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Shows the details of a single restaurant.Shows restaurant name, address, and coordinates.
 Has a scroll-able list that show all it's inspections by recency.
 Each inspection on the list should show: #critical issues, #non-critical issues, how long
 since inspection, and an icon with hazard level and colour.
  */
+class Inspection{
+    private int numCritIssues;
+    private int numNonCritIssues;
+    private String timeSinceInspection;
+    private int icon;
+
+    public Inspection(int numCritIssues,
+                      int numNonCritIssues,
+                      String timeSinceInspection,
+                      int icon){
+        this.numCritIssues = numCritIssues;
+        this.numNonCritIssues = numNonCritIssues;
+        this.timeSinceInspection = timeSinceInspection;
+        this.icon = icon;
+    }
+
+    public int getNumCritIssues() {
+        return numCritIssues;
+    }
+
+    public void setNumCritIssues(int numCritIssues) {
+        this.numCritIssues = numCritIssues;
+    }
+
+    public int getNumNonCritIssues() {
+        return numNonCritIssues;
+    }
+
+    public void setNumNonCritIssues(int numNonCritIssues) {
+        this.numNonCritIssues = numNonCritIssues;
+    }
+
+    public String getTimeSinceInspection() {
+        return timeSinceInspection;
+    }
+
+    public void setTimeSinceInspection(String timeSinceInspection) {
+        this.timeSinceInspection = timeSinceInspection;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+}
 class Restaurant{
     //temporary restaurant class, will be replaced with model package.
     private String name;
@@ -83,7 +134,7 @@ public class SingleRestaurantActivity extends AppCompatActivity {
             50,
             -123
             );
-
+    static List<Inspection> inspections = new ArrayList<Inspection>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,7 +149,14 @@ public class SingleRestaurantActivity extends AppCompatActivity {
         addressText_textview.setText(restaurant.getAddress());
         TextView coords_textView = (TextView)findViewById(R.id.coordinatesText);
         coords_textView.setText(restaurant.getLatitude() + ", " + restaurant.getLongitude());
-
+        inspections.add(new Inspection(2,
+                1,
+                "20 days",
+                0));
+        inspections.add(new Inspection(0,
+                0,
+                "1 year",
+                0));
     }
     public static Intent makeIntent(Context context) {
         return new Intent(context, SingleRestaurantActivity.class);
