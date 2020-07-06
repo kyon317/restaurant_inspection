3320af81e75b4d9a6ca403e81e6a80424a73eaff
quochuybui <qhbui@sfu.ca>
2020-07-06 16:07:59 -0700

    fix add

---
+++ app/src/main/java/ca/sfu/cmpt_276_project/UI/Restaurants.java
@@ -0,0 +1,74 @@
+package ca.sfu.cmpt_276_project.UI;
+
+import java.util.ArrayList;
+
+// Dummy object to work with listRestaurant layout
+
+public class Restaurants {
+
+    private String name;
+    private int icon;
+    private String hazard;
+    private int criticalViolationCount;
+    private String mostRecentDate;
+
+    private int hazardIcon;
+
+    public Restaurants(String name, int icon, int hazardIcon, String hazard, int criticalViolationCount, String mostRecentDate) {
+        this.name = name;
+        this.icon = icon;
+        this.hazardIcon = hazardIcon;
+        this.hazard = hazard;
+        this.criticalViolationCount = criticalViolationCount;
+        this.mostRecentDate = mostRecentDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getHazard() {
+        return hazard;
+    }
+
+    public void setHazard(String hazard) {
+        this.hazard = hazard;
+    }
+
+    public int getCriticalViolationCount() {
+        return criticalViolationCount;
+    }
+
+    public void setCriticalViolationCount(int criticalViolationCount) {
+        this.criticalViolationCount = criticalViolationCount;
+    }
+
+    public String getMostRecentDate() {
+        return mostRecentDate;
+    }
+
+    public void setMostRecentDate(String mostRecentDate) {
+        this.mostRecentDate = mostRecentDate;
+    }
+
+
+    public int getHazardIcon() {
+        return hazardIcon;
+    }
+
+    public void setHazardIcon(int hazardIcon) {
+        this.hazardIcon = hazardIcon;
+    }
+}
