bb6f0daf92fbf8d0ed64ad0e989b4f1fdb4f38a3
Vincent Hu <179036326@qq.com>
2020-07-28 02:11:42 +0800

    bugs fixed

--- app/src/main/java/ca/sfu/cmpt_276_project/UI/LoadingActivity.java
+++ app/src/main/java/ca/sfu/cmpt_276_project/UI/LoadingActivity.java
@@ -1,13 +1,15 @@
 /*Loading Activity loads data into RestaurantManager.
-* */
+ * */
 
 package ca.sfu.cmpt_276_project.UI;
 
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -17,11 +19,13 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -30,9 +34,11 @@ import ca.sfu.cmpt_276_project.CsvIngester.RestaurantCSVIngester;
 import ca.sfu.cmpt_276_project.Model.Restaurant;
 import ca.sfu.cmpt_276_project.Model.RestaurantManager;
 import ca.sfu.cmpt_276_project.R;
+import ca.sfu.cmpt_276_project.WebScraper.CSVDownloader;
 import ca.sfu.cmpt_276_project.WebScraper.DataManager;
 import ca.sfu.cmpt_276_project.WebScraper.DataStatus;
 import ca.sfu.cmpt_276_project.WebScraper.RunMode;
+import ca.sfu.cmpt_276_project.WebScraper.WebScraper;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -41,17 +47,37 @@ public class LoadingActivity extends AppCompatActivity {
     private RunMode runMode;
     private RestaurantManager restaurantManager;
     private boolean alertVisible = false;
+    private static final String TAG = "LoadingActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //READ/WRITE PERMISSION CODE: https://stackoverflow.com/a/51374641/8860660
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
         setContentView(R.layout.activity_loading);
         getSupportActionBar().setTitle("Surrey Restaurant Inspections");
+
+//        System.out.println("initial test: "+test);
+//        test();
+//        System.out.println("after first loop: "+test);
+//        test();
+//        System.out.println("second loop: "+test);
         dataManager = new DataManager();
         try {
             dataStatus = dataManager.checkForUpdates();
         } catch (ExecutionException | InterruptedException | ParseException | IOException e) {
             e.printStackTrace();
         }
+
         backgroundTask task = new backgroundTask();
         try {
             task.execute("selectRunMode").get();
@@ -68,12 +94,16 @@ public class LoadingActivity extends AppCompatActivity {
 
     public void selectRunMode() throws InterruptedException {
         if (!checkNetwork()){
-            Toast.makeText(this,"No Network Connection, loading from local directory",Toast.LENGTH_LONG).show();
-                if (dataManager.checkFileExistence(dataManager.getRestaurant_filename())&&dataManager.checkFileExistence(dataManager.getInspection_filename()))
-                    runMode = RunMode.LOCAL;
-                else
-                    runMode = RunMode.DEFAULT;
-        }else{
+            Toast.makeText(this,"No Network Connection, loading from local directory",
+                    Toast.LENGTH_LONG).show();
+
+            if (dataManager.checkFileExistence(dataManager.getRestaurant_filename()) &&
+                    dataManager.checkFileExistence(dataManager.getInspection_filename()))
+                runMode = RunMode.LOCAL;
+            else
+                runMode = RunMode.DEFAULT;
+        }
+        else{
             if (dataStatus != DataStatus.UP_TO_DATE){
                 alertVisible = true;
             }
@@ -83,6 +113,7 @@ public class LoadingActivity extends AppCompatActivity {
 
     public boolean checkNetwork(){
         boolean isConnected = false;
+
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
                 connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
@@ -93,19 +124,20 @@ public class LoadingActivity extends AppCompatActivity {
             isConnected = false;
         return isConnected;
     }
-    public void setRestaurantManager(RunMode selectedRunmMode){
+
+    public void setRestaurantManager(RunMode selectedRunMode){
         List<Restaurant> restaurantList = new ArrayList<>();
         RestaurantCSVIngester restaurantImport = new RestaurantCSVIngester();
         try {
-            if (selectedRunmMode == RunMode.LOCAL) {
+            if (selectedRunMode == RunMode.LOCAL) {
                 System.out.println("LOADING LOCAL DATA");
                 restaurantImport.readRestaurantList(this, dataManager.getDirectory_path()+dataManager.getRestaurant_filename(), 1);
                 restaurantList = restaurantImport.getRestaurantList();
-            } else if (selectedRunmMode == RunMode.DEFAULT){
+            } else if (selectedRunMode == RunMode.DEFAULT){
                 System.out.println("LOADING DEFAULT DATA");
                 restaurantImport.readRestaurantList(this,null, 0);
                 restaurantList = restaurantImport.getRestaurantList();
-            }else if (selectedRunmMode == RunMode.UPDATE){
+            }else if (selectedRunMode == RunMode.UPDATE){
                 System.out.println("DOWNLOADING DATA");
                 dataManager.downloadList(this,dataManager.getRestaurant_url(),dataManager.getRestaurant_filename(),dataManager.getRestaurant_csv_url());
                 restaurantImport.readRestaurantList(this, dataManager.getDirectory_path()+dataManager.getRestaurant_filename(), 1);
@@ -120,13 +152,13 @@ public class LoadingActivity extends AppCompatActivity {
         //get Inspection Data of Restaurants from CSV
         InspectionDataCSVIngester inspectionDataImport = new InspectionDataCSVIngester();
         try {
-            if (selectedRunmMode== RunMode.LOCAL) {
+            if (selectedRunMode== RunMode.LOCAL) {
                 System.out.println("LOADING LOCAL DATA");
                 inspectionDataImport.readInspectionData(this, dataManager.getDirectory_path()+dataManager.getInspection_filename(), 1);
-            } else if (selectedRunmMode == RunMode.DEFAULT){
+            } else if (selectedRunMode == RunMode.DEFAULT){
                 System.out.println("LOADING DEFAULT DATA");
                 inspectionDataImport.readInspectionData(this, null, 0);
-            }else if (selectedRunmMode == RunMode.UPDATE){
+            }else if (selectedRunMode == RunMode.UPDATE){
                 System.out.println("DOWNLOADING DATA");
                 dataManager.downloadList(this,dataManager.getInspection_url(),dataManager.getInspection_filename(),dataManager.getInspection_csv_url());
                 inspectionDataImport.readInspectionData(this, dataManager.getDirectory_path()+dataManager.getInspection_filename(), 1);
@@ -151,7 +183,6 @@ public class LoadingActivity extends AppCompatActivity {
         Intent intent = new Intent(this,MapsActivity.class);
         startActivity(intent);
     }
-
 
     public class backgroundTask extends AsyncTask<String,Integer,Integer>{
 
@@ -180,7 +211,15 @@ public class LoadingActivity extends AppCompatActivity {
                         LoadingActivity.this.startMapActivity();
                         count = 1;
                         break;
-                    case "setRestaurantManager":
+                    case "Download":
+                        try {
+                            dataManager.downloadAll(LoadingActivity.this);
+                        } catch (ExecutionException | InterruptedException | IOException e) {
+                            e.printStackTrace();
+                        }
+                        count = 2;
+                        break;
+                    case "Load":
                         setRestaurantManager(RunMode.LOCAL);
                         break;
                     default:
@@ -188,6 +227,7 @@ public class LoadingActivity extends AppCompatActivity {
                         break;
                 }
                 publishProgress(count);
+
             }
             System.out.println("current runmode: "+runMode);
             return count;
@@ -205,9 +245,9 @@ public class LoadingActivity extends AppCompatActivity {
             super.onPostExecute(integer);
             if (integer==1) setRestaurantManager(RunMode.LOCAL);
             if (integer==0&&runMode!=RunMode.UPDATE) setRestaurantManager(runMode);
-            //if (values[0]==2) startMapActivity();
         }
     }
+
     public void showDialog(){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
@@ -217,16 +257,30 @@ public class LoadingActivity extends AppCompatActivity {
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        setRestaurantManager(RunMode.UPDATE);
-                        while (true){
-
-                            File DummyFile = new File(dataManager.getDirectory_path()+dataManager.getInspection_filename());
-                            if (DummyFile.length()>=2289280){
-                                break;
-                            }
+                        CSVDownloader res_downloader = new CSVDownloader(dataManager.getRestaurant_filename(),LoadingActivity.this);
+                        CSVDownloader ins_downloader = new CSVDownloader(dataManager.getInspection_filename(),LoadingActivity.this);
+                        String[] res_csv = new String[2];
+                        String[] ins_csv = new String[2];
+                        try {
+                            res_csv = new WebScraper().execute(dataManager.getRestaurant_url()).get();
+                            ins_csv = new WebScraper().execute(dataManager.getInspection_url()).get();
+                        } catch (ExecutionException | InterruptedException e) {
+                            e.printStackTrace();
                         }
-                        setRestaurantManager(RunMode.LOCAL);
-                        startMapActivity();
+                        res_downloader.execute(res_csv[0]);
+                        ins_downloader.execute(ins_csv[0]);
+                        Date currentTimeStamp = new Date();
+                        currentTimeStamp.getTime();
+                        try {
+                            dataManager.updateLocalDate("inspectionreports_date_local.txt",currentTimeStamp.toString());
+                            dataManager.updateLocalDate("restaurants_date_local.txt",currentTimeStamp.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        backgroundTask loading_task = new backgroundTask();
+                        loading_task.execute("Load");
+                        backgroundTask startMap_task = new backgroundTask();
+                        startMap_task.execute("startMapActivity");
                     }
                 })
                 .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
@@ -240,5 +294,6 @@ public class LoadingActivity extends AppCompatActivity {
         alertDialog.show();
         alertVisible = false;
     }
+
 
 }
